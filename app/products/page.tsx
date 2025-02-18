6fd537dd4ec17c664aa5ea64fbb3f0aea75f18df
mhridoy <35825505+mhridoy@users.noreply.github.com>
2025-02-18 16:10:29 +0300

    Update product images to include all available ferrous and non-ferrous images

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,16 +5,17 @@ import { useRef, useState } from "react"
 import Image from "next/image"
 
 const getImagesForCategory = () => {
-  // Ferrous images are numbered 1-20
-  const ferrousImages = Array.from({ length: 20 }, (_, i) => {
-    const num = i + 1
-    return `/Our Products/ferrous_images/image${num}.jpeg`
-  })
+  // Ferrous images (using .png extension)
+  const ferrousImageNumbers = [1, 2, 3, 5, 7, 8, 10]
+  const ferrousImages = ferrousImageNumbers.map(num => 
+    `/Our Products/ferrous_images/image${num}.png`
+  )
 
-  // Non-ferrous images have specific numbers
+  // Non-ferrous images (using .jpeg extension)
   const nonFerrousImageNumbers = [
-    1, 3, 4, 10, 24, 27, 29, 30, 35, 37, 38, 39, 40, 41,
-    54, 58, 59, 60, 65, 72, 73
+    1, 3, 4, 10, 11, 12, 13, 16, 17, 18, 20, 21, 22, 23, 24, 26, 27, 29, 30,
+    31, 32, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 48, 49, 50, 52, 53, 54,
+    55, 56, 58, 59, 60, 63, 65, 66, 68, 72, 73, 100
   ]
   const nonFerrousImages = nonFerrousImageNumbers.map(num => 
     `/Our Products/nonferrous_images/image${num}.jpeg`
